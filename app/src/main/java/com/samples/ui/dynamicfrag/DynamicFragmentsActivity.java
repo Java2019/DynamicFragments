60942ae7b6259b585c307f533ec91c81d621a216
java2019 <java_2019@mail.ru>
2017-06-19 12:18:20 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/ui/dynamicfrag/DynamicFragmentsActivity.java
@@ -0,0 +1,13 @@
+package com.samples.ui.dynamicfrag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DynamicFragmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_fragments);
+    }
+}
